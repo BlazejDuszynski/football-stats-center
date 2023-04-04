45ebabd760e57617df56bc642bca2405d26eb00e
Blazej Duszynski <bduszynski92@gmail.com>
2023-04-04 19:39:17 +0200

    added on hover on tableItem effect

--- src/Components/Pages/Search/TableItem.js
+++ src/Components/Pages/Search/TableItem.js
@@ -5,12 +5,16 @@ const TableItem = (props) => {
   return (
     <tr className={classes.tableRow}>
       <td className={classes.centeredData}>{props.position + "."}</td>
-      <td className={classes.teamName}>{props.name}</td>
+      <td className={classes.teamName}>
+        <p className={classes.teamName__text}>{props.name}</p>
+      </td>
       <td className={classes.centeredData}>{props.games}</td>
       <td className={classes.centeredData}>{props.won}</td>
       <td className={classes.centeredData}>{props.draw}</td>
       <td className={classes.centeredData}>{props.lost}</td>
-      <td className={classes.centeredData}>{props.goals_scored + "-" + props.goals_against}</td>
+      <td className={classes.centeredData}>
+        {props.goals_scored + "-" + props.goals_against}
+      </td>
       <td className={classes.centeredData}>{props.points}</td>
     </tr>
   );
