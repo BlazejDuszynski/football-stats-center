6738d6073d3fbd6975a0e3099099762fed0640c0
Blazej Duszynski <bduszynski92@gmail.com>
2023-04-06 20:36:27 +0200

    creted SquadItem.js component

---
+++ src/Components/Pages/Team/SquadItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SquadItem = () => {
+  return (
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  );
+};
+
+export default SquadItem;
